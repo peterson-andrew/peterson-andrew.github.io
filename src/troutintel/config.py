7686e6d6f9bf3d084584228d890ba6ed408ac7a2
peterson-andrew <78775011+peterson-andrew@users.noreply.github.com>
2026-06-18 19:50:25 -0400

    Update config.py

--- src/troutintel/config.py
+++ src/troutintel/config.py
@@ -1,1 +1,14 @@
+from typing import Any, Dict
+from troutintel.io import load_json
 
+RIVER_CONFIG_PATH = "data/config/river_config.json
+
+def load_river_config() -> Dict[str, Any]:
+  return load_json(
+    RIVER_CONFIG_PATH
+  )
+
+def get_river(river_name: str,) -> Dict[str, Any]:
+  rivers = load_river_config()
+  return rivers[river_name]
+  
