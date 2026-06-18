ab01b9392751a5177245012803c14b658cb08ac4
peterson-andrew <78775011+peterson-andrew@users.noreply.github.com>
2026-06-18 12:29:20 -0400

    Update io.py

--- src/troutintel/io.py
+++ src/troutintel/io.py
@@ -1,1 +1,26 @@
+import json
+from pathlib import Path
+from typing import Any
 
+def load_json(path: str) -> Any:
+  return json.loads(
+    Path(path).read_text(
+      encoding="utf-8"
+    )
+  )
+
+def save_json(data: Any, output_path: str,) -> None:
+  path = Path(output_path)
+
+  path.parent.mkdir(
+    parents=True,
+    exist_ok=True,
+  )
+
+path.write_text(
+  json.dumps(
+    data,
+    indent=2,
+  ),
+  encoding="utf-8",
+)
